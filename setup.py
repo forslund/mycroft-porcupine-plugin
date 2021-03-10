86ceda54dd38562bdbe282b63f8fd277f2339468
Åke Forslund <ake.forslund@gmail.com>
2021-03-10 21:26:21 +0100

    Update setup.py
    - Add long description
    - Fix version number

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 PLUGIN_ENTRY_POINT = ('porcupine_wakeword_plug = '
                       'mycroft_porcupine_plugin:PorcupineWakeword')
 
 setup(
     name='mycroft-porcupine-plugin',
-    version='1',
-    description='A Wakeword plugin for mycroft',
+    version='0.1',
+    description='A Porcupine wakeword plugin for mycroft',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Åke Forslund',
     author_email='ake.forslund@gmail.com',
     packages=['mycroft_porcupine_plugin'],
